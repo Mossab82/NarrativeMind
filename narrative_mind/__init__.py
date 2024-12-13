4347b14c8c67760e7761c31e7566b76dc0e721df
Mossab Ibrahim <47082589+Mossab82@users.noreply.github.com>
2024-12-13 15:39:30 +0300

    Create __init__.py

---
+++ narrative_mind/__init__.py
@@ -0,0 +1,6 @@
+from narrative_mind.models.hybrid_model import NarrativeMind
+from narrative_mind.models.neural import NeuralGenerator
+from narrative_mind.models.pattern_matcher import PatternMatcher
+from narrative_mind.models.dialect_adapter import DialectAdapter
+
+__version__ = "1.0.0"
