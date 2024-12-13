9b7c219ec5b0f15ffb9358c43498a9067ac1aad9
Mossab Ibrahim <47082589+Mossab82@users.noreply.github.com>
2024-12-13 15:48:54 +0300

    Create __init__.py

---
+++ narrative_mind/evaluation/__init__.py
@@ -0,0 +1,5 @@
+from .metrics import NarrativeEvaluator
+from .cultural_metrics import CulturalMetrics
+from .dialect_metrics import DialectMetrics
+
+__all__ = ['NarrativeEvaluator', 'CulturalMetrics', 'DialectMetrics']
